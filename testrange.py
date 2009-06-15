8789d592b279cb7f79fe193b4b60abda34fcc9e1
Jean Azzopardi <lotrlordillidan@gmail.com>
2009-06-15 17:38:11 +0200

    Added a python program to test ranges

---
+++ testrange.py
@@ -0,0 +1,5 @@
+start = int(input("Enter the start of the range: "))
+end = int(input("Enter the end of the range: "))
+step = int(input("Enter the step of the range: "))
+for i in range(start,end,step):
+	print(i)
